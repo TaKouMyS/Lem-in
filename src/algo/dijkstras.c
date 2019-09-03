08a5a78a643dac87dcee6304650448304c793a09
Finola CAHILL <fcahill@e1r4p17.42.fr>
2019-09-03 19:06:39 +0200

    comments added to dijkstras algo

--- src/algo/dijkstras.c
+++ src/algo/dijkstras.c
@@ -4,7 +4,7 @@
 #include<math.h>
 #include<stdlib.h>
 
-int *fill_dist(int *dist, int length)
+static int *fill_dist(int *dist, int length)
 {
     int i;
 
@@ -17,14 +17,14 @@ int *fill_dist(int *dist, int length)
     return (dist);
 }
 
-int node_is_linked(int i)
+static int node_is_linked(int i)
 {
     return (i);
 }
 
-void check_minimum_distance(int current, int saved, int *dist, int *path)
+static void check_minimum_distance(int current, int saved, int *dist, int *path)
 {
-    if (dist[current] + 1 < dist[saved])
+    if (dist[current] + 1 < dist[saved]) //checking if this distance from source of the linking node + an additional link is smaller than saved distance, as we're looking for the minimum path
     {
         
         dist[saved] = dist[current] + 1;
@@ -32,7 +32,7 @@ void check_minimum_distance(int current, int saved, int *dist, int *path)
     }
 }
 
-void print_reversed_path(int *path, int length)
+static void print_reversed_path(int *path, int length)
 {
     while (length != 0)
     {
@@ -41,7 +41,7 @@ void print_reversed_path(int *path, int length)
     }
 }
    
-int dijsktra(int map[][7], int length)
+static int dijsktra(int map[][7], int length)
 {
     int *distance;
     int *path;
@@ -50,101 +50,49 @@ int dijsktra(int map[][7], int length)
 
     if (!(path = malloc(sizeof(int) * length)))
         return (-1);
-    if ((distance = fill_dist(distance, length)) == NULL)
+    if ((distance = fill_dist(distance, length)) == NULL) //filling distance with arbitary large numbers (1000 in this case), apart from the first element
         return (-1);
     while (++i < length)
     {
         j = -1;
         while(++j < length)
         {
-            if (node_is_linked(map[i][j]))
+            if (node_is_linked(map[i][j])) //this will be a 1 if theres a link or 0 if there's no link
                 check_minimum_distance(i, j, distance, path);
         }
     }
-    print_reversed_path(path, length - 1);
+    print_reversed_path(path, length - 1); //printing from the end backwards, because it's easier, but easy to fix if need be. 
     return (0);
 }
 
 int main()
 {
+    /*current way of dealing with links. taking a look at map[0] - the 1 at map[0][1] indicates there is a link between map[0] and map[1], the 0 at map[0][3] indicates no link between map[0] and map[3]
+    This map translates to: 
+      
+         1   ----------3
+      | \    \          \ 
+      |  \    \          \
+      |   \    \         6
+      |    \    \        /
+      |     \    \      / 
+      |      \    \    /
+      |       \    \  /
+      0--------2-4--5/
+
+      We're also working under the presumption that 0 is always start and that the last element of the map is always end.*/
     int map[7][7] = 
     {
-        {0, 1, 1, 0, 0, 0, 0},
+        {0, 1, 1, 0, 0, 0, 1},
         {0, 0, 1, 1, 0, 1, 0},
         {1, 1, 0, 0, 1, 0, 0},
         {0, 1, 0, 0, 0, 0, 1},
         {0, 0, 1, 0, 0, 1, 0},
         {0, 1, 0, 0, 1, 0, 6}
     };
-    int *path;
-
-
-    /*
-    int cost[N][N],i,j,w,ch,co;
-    int source, target,x,y;
-    printf("\t The Shortest Path Algorithm ( DIJKSTRA'S ALGORITHM in C \n\n");
-    for(i=1;i< N;i++)
-    for(j=1;j< N;j++)
-    cost[i][j] = IN;
-    for(x=1;x< N;x++)
-    {
-        for(y=x+1;y< N;y++)
-        {
-            printf("Enter the weight of the path between nodes %d and %d: ",x,y);
-            scanf("%d",&w);
-            cost [x][y] = cost[y][x] = w;
-        }
-        printf("\n");
-    }
-    printf("\nEnter the source:");
-    scanf("%d", &source);
-    printf("\nEnter the target");
-    scanf("%d", &target); */
     dijsktra(map, 7);
-    // printf("\nThe Shortest Path: %d",path);
+    return (0);
 }
 
 
-    
-    /*
-    for(i=1;i< N;i++)
-    {
-        dist[i] = IN;
-        prev[i] = -1;
-    }
-    start = source;
-    selected[start]=1;
-    dist[start] = 0;
-    while(selected[target] ==0)
-    {
-        min = IN;
-        m = 0;
-        for(i=1;i< N;i++)
-        {
-            d = dist[start] +cost[start][i];
-            if(d< dist[i]&&selected[i]==0)
-            {
-                dist[i] = d;
-                prev[i] = start;
-            }
-            if(min>dist[i] && selected[i]==0)
-            {
-                min = dist[i];
-                m = i;
-            }
-        }
-        start = m;
-        selected[start] = 1;
-    }
-    start = target;
-    j = 0;
-    while(start != -1)
-    {
-        path[j++] = start+65;
-        start = prev[start];
-    }
-    path[j]='\0';
-    strrev(path);
-    printf("%s", path);
-    return dist[target];
-    */
+   
