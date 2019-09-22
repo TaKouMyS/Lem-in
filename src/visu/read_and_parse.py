43fcb19d20785cc6667cb3c3f33793cc40cd82fa
Allan Mamy <amamy@student.42.fr>
2019-09-23 01:01:50 +0200

    Parsing : all data read and store, first functionnal.
    **read_and_parse**I
    	ant_number is now recorded
    	moves are recorded as well

--- src/visu/read_and_parse.py
+++ src/visu/read_and_parse.py
@@ -6,7 +6,7 @@
 #    By: amamy <amamy@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/09/19 02:22:52 by amamy             #+#    #+#              #
-#    Updated: 2019/09/20 01:23:45 by amamy            ###   ########.fr        #
+#    Updated: 2019/09/23 01:01:14 by amamy            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,22 +20,26 @@ class farm:
     """ This class represent the content of the farm."""
 
     def __init__(self):
-        self.links = []
-        self.nodes = []
-        self.start = None
-        self.end  = None
-        
+        self.links  = []
+        self.nodes  = []
+        self.start  = None
+        self.end    = None
+        self.ants   = int(0)
 
     
 def farm_parser():
-    input_list=[]
     i = 0   
+    input_list, moves = [], []
     ant_farm = farm()
     for line in sys.stdin:
         input_list.append((line.strip()).split())
+        if line[0][0] == 'L':
+            moves.append(line.strip().split())
         i += 1
     len_input_list = i
     i = 1
+    ant_farm.ants = input_list[0][0]
+    print ant_farm.ants
     while i < len_input_list and re.search(r"()-()", input_list[i][0]) is None:
         if len(input_list[i]) == 3:
             ant_farm.nodes.append(input_list[i][0])
@@ -48,9 +52,11 @@ def farm_parser():
     while i < len_input_list and re.search(r"()-()", input_list[i][0]):
         ant_farm.links.append(input_list[i][0].split('-'))
         i += 1
+    print input_list
+    print moves
     return ant_farm
 
-ant_farm = parser()
+ant_farm = farm_parser()
 g=nx.Graph()
 g.add_nodes_from(ant_farm.nodes)
 g.add_edges_from(ant_farm.links)
